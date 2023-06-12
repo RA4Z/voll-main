52e47c3c2baf342335b05f8c25efcf99c66245a9
RA4Z <rraz639@gmail.com>
2023-06-12 12:28:33 -0300

    Criação do título do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Container } from '@mui/material';
 import './App.css';
 import Cabecalho from './components/Cabecalho';
+import Titulo from './components/Titulo';
 
 function App() {
   return (
     <>
       <Cabecalho />
       <Container>
-        
+        <Titulo>Área Administrativa</Titulo>
       </Container>
     </>
   );
