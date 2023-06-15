a0542b1fbfb0b5907e33f480f77144101f2ee3b5
RA4Z <rraz639@gmail.com>
2023-06-15 12:24:54 -0300

    Estilizando os gráficos com styled components

--- src/components/Grafico/index.tsx
+++ src/components/Grafico/index.tsx
@@ -1,11 +1,8 @@
-import { ResponsiveContainer } from "recharts";
-import { XAxis } from "recharts/types/cartesian/XAxis";
-import { BarChart } from "recharts/types/chart/BarChart";
-import { YAxis } from "recharts/types/cartesian/YAxis";
-import { Bar } from "recharts/types/cartesian/Bar";
+import { ResponsiveContainer, XAxis, BarChart, YAxis, Bar } from "recharts";
 import IProfissional from "../../types/IProfissional";
 import IConsulta from "../../types/IConsulta";
 import useDadosGrafico from "./useDadosGrafico";
+import styled from "styled-components";
 
 interface Props {
     profissionais: IProfissional[] | null,
@@ -17,16 +14,23 @@ interface IDados {
     consultas: number
 }
 
+const SecaoEstilizada = styled.section`
+    background-color: var(--branco);
+    border-radius: 16px;
+`
+
 function Grafico({ profissionais, consultas }: Props) {
     let dados: Array<IDados> = useDadosGrafico({ profissionais, consultas });
     return (
-        <ResponsiveContainer width='100%' height={350}>
-            <BarChart layout='vertical' data={dados} margin={{ top: 25, right: 40, left: 40, bottom: 20 }} >
-                <XAxis type='number' ></XAxis>
-                <YAxis type='category' datakey='nome' ></YAxis>
-                <Bar datakey='consultas' fill='#083860' barSize={30}></Bar>
-            </BarChart>
-        </ResponsiveContainer>
+        <SecaoEstilizada>
+            <ResponsiveContainer width='100%' height={350}>
+                <BarChart layout='vertical' data={dados} margin={{ top: 25, right: 40, left: 40, bottom: 20 }} >
+                    <XAxis type='number' ></XAxis>
+                    <YAxis type='category' dataKey='nome' ></YAxis>
+                    <Bar dataKey='consultas' fill='#083860' barSize={30}></Bar>
+                </BarChart>
+            </ResponsiveContainer>
+        </SecaoEstilizada>
     )
 }
 
