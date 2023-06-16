502abfff851091cf3c1cab7f82b4f32757d20983
RA4Z <rraz639@gmail.com>
2023-06-16 12:44:53 -0300

    Finalizando estilização com títulos e subtitulos

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import useDadosConsulta from './useDadosConsulta';
 import Grafico from './components/Grafico';
 import useDadosProfissional from './useDadosProfissional';
 import Avaliacao from './components/Avaliacao';
+import Botao from './components/Botao';
+import Subtitulo from './components/Subtitulo';
 
 function App() {
   const { dados: consultas, erro: consultasErro } = useDadosConsulta();
@@ -22,7 +24,13 @@ function App() {
       <Cabecalho />
       <Container>
         <Titulo>Área Administrativa</Titulo>
+        <Botao>Cadastrar especialista</Botao>
+        <Titulo>Consultas do dia</Titulo>
         <Tabela consultas={consultas} />
+        <Botao>Ver mais</Botao>
+        <Titulo>Consultas mensais por especialista</Titulo>
+        <Subtitulo>Dezembro/22</Subtitulo>
+
         <Grafico consultas={consultas} profissionais={profissionais} />
         <Avaliacao profissionais={profissionais} />
       </Container>
--- src/components/Avaliacao/index.tsx
+++ src/components/Avaliacao/index.tsx
@@ -1,6 +1,9 @@
 import Card from './Card'
 import IProfissional from "../../types/IProfissional"
 import styled from 'styled-components'
+import Subtitulo from '../Subtitulo'
+import Titulo from '../Titulo'
+import Botao from '../Botao'
 
 const SecaoCard = styled.section`
     display:flex;
@@ -11,11 +14,16 @@ const SecaoCard = styled.section`
 
 function Avaliacao({ profissionais }: { profissionais: IProfissional[] | null }) {
     return (
-        <SecaoCard>
-            {profissionais?.map((profissional) => {
-                return <Card profissional={profissional} />
-            })}
-        </SecaoCard>
+        <>
+            <Titulo>Avaliações de especialistas</Titulo>
+            <Subtitulo>Dezembro/22</Subtitulo>
+            <SecaoCard>
+                {profissionais?.map((profissional) => {
+                    return <Card profissional={profissional} />
+                })}
+            </SecaoCard>
+            <Botao>Ver mais</Botao>
+        </>
     )
 }
 export default Avaliacao
